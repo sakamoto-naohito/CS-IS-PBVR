7ed15c16b79a4168f42a814b680435b7635323bd
sakamoto-naohito <sakamoto.naohito@jaea.go.jp>
2026-09-03 15:11:08 +0900

    Server KVSMLConverter 入力データの拡張子の識別処理を改善
    - Paraviewで自動で連番ファイル認識できるファイル名のパターンの場合は拡張子を識別できるように修正

--- Server/KVSMLConverter/Example/example.cpp
+++ Server/KVSMLConverter/Example/example.cpp
@@ -90,69 +90,130 @@ int main( int argc, char** argv )
         return -1;
     }
 
-    fs::path input_file_extension = input_file_path.extension();
-    fs::path input_filename_without_extension = input_file_path.filename().stem();
+    // 拡張子と拡張子なしのファイル名を取得する
+    // 対応している拡張子
+    static const std::vector<std::string> supported_extensions = {
+        ".stl",
+        ".vtp",
+        // 現在非対応 ".xyz",
+        // 現在非対応 ".vtr",
+        ".vtk",
+        ".vti",
+        ".vts",
+        ".pvts",
+        ".inp",
+        ".vtu",
+        ".pvtu",
+        ".vtm",
+        ".case",
+        ".nc",
+        ".ncdf"
+    };
 
-    if ( input_file_extension == ".stl" )
+    std::optional<std::string> selected_extension;        // 使用される拡張子を格納する
+    fs::path input_filename = input_file_path.filename(); // パスを除いたファイル名
+    std::string input_filename_without_extension;         // パスを除いたファイル名から拡張子と.と_を除いた文字列
+
+    for ( const auto& extension : supported_extensions )
+    {
+        // ファイル名に対応拡張子が存在するか   
+        if ( input_filename.string().find( extension ) == std::string::npos )
+        {
+            continue;
+        }
+
+        // 最も長い拡張子を採用する
+        if ( !selected_extension || extension.size() > selected_extension->size() )
+        {
+            selected_extension = extension;
+        }
+    }
+
+    // 対応していない拡張子の場合、対応していないことを表示し終了
+    if ( !selected_extension )
+    {
+        std::cout << "This file extension is not yet supported" << std::endl;
+        return -1;
+    }
+
+    // 採用した拡張子よりも前の文字列と後ろの文字列を取得する
+    const auto pos = input_filename.string().find( selected_extension.value() );
+    const std::string before = input_filename.string().substr( 0, pos );                          // 拡張子より前
+    const std::string after  = input_filename.string().substr( pos + selected_extension->size() ); // 拡張子より後ろ
+
+    // 拡張子を除いて前後を結合
+    input_filename_without_extension = before + after;
+
+    // '.' と '_' を削除
+    input_filename_without_extension.erase(
+        std::remove_if(
+            input_filename_without_extension.begin(),
+            input_filename_without_extension.end(),
+            [](char c)
+            {
+                return c == '.' || c == '_' || c == '*';
+            }),
+        input_filename_without_extension.end()
+    );
+
+    if ( selected_extension == ".stl" )
     {
         std::string separator(1, fs::path::preferred_separator);
-        std::string output_file_path = output_directory_path.string() + separator + input_filename_without_extension.string() + ".kvsml";
+        std::string output_file_path = output_directory_path.string() + separator + input_filename_without_extension + ".kvsml";
         Stl2Kvsml( output_file_path, input_file_path.string() );
     }
-    else if ( input_file_extension == ".vtp" )
+    else if ( selected_extension == ".vtp" )
     {
         std::string separator(1, fs::path::preferred_separator);
-        std::string output_file_path = output_directory_path.string() + separator + input_filename_without_extension.string() + ".kvsml";
+        std::string output_file_path = output_directory_path.string() + separator + input_filename_without_extension + ".kvsml";
         Vtp2Kvsml( output_file_path, input_file_path.string() );
     }
     /*
-    else if ( input_file_extension == ".xyz" || input_file_extension == ".q" || input_file_extension == ".f" )
+    else if ( selected_extension == ".xyz" || selected_extension == ".q" || selected_extension == ".f" )
     {
         fs::path input_file_path_xyz = input_file_path.replace_extension(".xyz");
         fs::path input_file_path_q = input_file_path.replace_extension(".q");
         fs::path input_file_path_f = input_file_path.replace_extension(".f");
-        Plot3d2Kvsml( output_directory_path.string(), input_filename_without_extension.string(), input_file_path_xyz.string(), input_file_path_q.string(), input_file_path_f.string() );
+        Plot3d2Kvsml( output_directory_path.string(), input_filename_without_extension, input_file_path_xyz.string(), input_file_path_q.string(), input_file_path_f.string() );
     }
-    else if ( input_file_extension == ".vtr" )
+    else if ( selected_extension == ".vtr" )
     {
-        Vtr2Kvsml( output_directory_path.string(), input_filename_without_extension.string(), input_file_path.string() );
+        Vtr2Kvsml( output_directory_path.string(), input_filename_without_extension, input_file_path.string() );
     }
     */
-    else if ( input_file_extension == ".vtk" )
+    else if ( selected_extension == ".vtk" )
     {
-        StructuredPoints2Kvsml( output_directory_path.string(), input_filename_without_extension.string(), input_file_path.string() );
+        StructuredPoints2Kvsml( output_directory_path.string(), input_filename_without_extension, input_file_path.string() );
     }
-    else if ( input_file_extension == ".vti" )
+    else if ( selected_extension == ".vti" )
     {
         if ( contains_wildcard( input_file_path.string() ) )
         {
-            std::string input_filename_removed_wildcard = input_filename_without_extension.string().substr(0, input_filename_without_extension.string().length() - 2);
-            SeriesVti2Kvsml( output_directory_path.string(), input_filename_removed_wildcard, input_file_path.generic_string() );
+            SeriesVti2Kvsml( output_directory_path.string(), input_filename_without_extension, input_file_path.generic_string() );
         }
         else
         {
-            Vti2Kvsml( output_directory_path.string(), input_filename_without_extension.string(), input_file_path.string() );
+            Vti2Kvsml( output_directory_path.string(), input_filename_without_extension, input_file_path.string() );
         }
     }
-    else if ( input_file_extension == ".vts" )
+    else if ( selected_extension == ".vts" )
     {
-        Vts2Kvsml( output_directory_path.string(), input_filename_without_extension.string(), input_file_path.string() );
+        Vts2Kvsml( output_directory_path.string(), input_filename_without_extension, input_file_path.string() );
     }
-    else if ( input_file_extension == ".pvts" )
+    else if ( selected_extension == ".pvts" )
     {
-        Pvts2Kvsml( output_directory_path.string(), input_filename_without_extension.string(), input_file_path.string() );
+        Pvts2Kvsml( output_directory_path.string(), input_filename_without_extension, input_file_path.string() );
     }
 
-    else if ( input_file_extension == ".inp" )
+    else if ( selected_extension == ".inp" )
     {
-        AvsUcd2Kvsml( output_directory_path.string(), input_filename_without_extension.string(), input_file_path.string() );
+        AvsUcd2Kvsml( output_directory_path.string(), input_filename_without_extension, input_file_path.string() );
     }
-    else if ( input_file_extension == ".vtu" )
+    else if ( selected_extension == ".vtu" )
     {
         if ( contains_wildcard( input_file_path.string() ) )
         {
-            std::string input_filename_removed_wildcard = input_filename_without_extension.string().substr(0, input_filename_without_extension.string().length() - 2);
-            SeriesVtu2Kvsml( output_directory_path.string(), input_filename_removed_wildcard, input_file_path.generic_string() );
+            SeriesVtu2Kvsml( output_directory_path.string(), input_filename_without_extension, input_file_path.generic_string() );
         }
         else
         {
@@ -161,53 +222,51 @@ int main( int argc, char** argv )
             if ( input_vtu.isPointObjectConvertible() )
             {
                 std::string separator(1, fs::path::preferred_separator);
-                std::string output_file_path = output_directory_path.string() + separator + input_filename_without_extension.string() + ".kvsml";
+                std::string output_file_path = output_directory_path.string() + separator + input_filename_without_extension + ".kvsml";
                 PointVtu2Kvsml( output_file_path, input_file_path.string() );
             }
             else if ( input_vtu.isLineObjectConvertible() )
             {
                 std::string separator(1, fs::path::preferred_separator);
-                std::string output_file_path = output_directory_path.string() + separator + input_filename_without_extension.string() + ".kvsml";
+                std::string output_file_path = output_directory_path.string() + separator + input_filename_without_extension + ".kvsml";
                 LineVtu2Kvsml( output_file_path, input_file_path.string() );
             }
             else if ( input_vtu.isPolygonObjectConvertible() )
             {
                 std::string separator(1, fs::path::preferred_separator);
-                std::string output_file_path = output_directory_path.string() + separator + input_filename_without_extension.string() + ".kvsml";
+                std::string output_file_path = output_directory_path.string() + separator + input_filename_without_extension + ".kvsml";
                 TriangleVtu2Kvsml( output_file_path, input_file_path.string() );
             }
             else
             {
-                Vtu2Kvsml( output_directory_path.string(), input_filename_without_extension.string(), input_file_path.string() );
+                Vtu2Kvsml( output_directory_path.string(), input_filename_without_extension, input_file_path.string() );
             }
         }
     }
-    else if ( input_file_extension == ".pvtu" )
+    else if ( selected_extension == ".pvtu" )
     {
         if ( contains_wildcard( input_file_path.string() ) )
         {
-            std::string input_filename_removed_wildcard = input_filename_without_extension.string().substr(0, input_filename_without_extension.string().length() - 2);
-            SeriesPvtu2Kvsml( output_directory_path.string(), input_filename_removed_wildcard, input_file_path.generic_string() );
-            // SeriesPvtu2KvsmlWhole( output_directory_path.string(), input_filename_removed_wildcard, input_file_path.generic_string() );
+            SeriesPvtu2Kvsml( output_directory_path.string(), input_filename_without_extension, input_file_path.generic_string() );
+            // SeriesPvtu2KvsmlWhole( output_directory_path.string(), input_filename_without_extension, input_file_path.generic_string() );
         }
         else
         {
-            Pvtu2Kvsml( output_directory_path.string(), input_filename_without_extension.string(), input_file_path.string() );
+            Pvtu2Kvsml( output_directory_path.string(), input_filename_without_extension, input_file_path.string() );
         }
     }
-    else if ( input_file_extension == ".vtm" )
+    else if ( selected_extension == ".vtm" )
     {
         if ( contains_wildcard( input_file_path.string() ) )
         {
-            std::string input_filename_removed_wildcard = input_filename_without_extension.string().substr(0, input_filename_without_extension.string().length() - 2);
-            SeriesVtm2Kvsml( output_directory_path.string(), input_filename_removed_wildcard, input_file_path.generic_string() );
+            SeriesVtm2Kvsml( output_directory_path.string(), input_filename_without_extension, input_file_path.generic_string() );
         }
         else
         {
-            Vtm2Kvsml( output_directory_path.string(), input_filename_without_extension.string(), input_file_path.string() );
+            Vtm2Kvsml( output_directory_path.string(), input_filename_without_extension, input_file_path.string() );
         }
     }
-    else if ( input_file_extension == ".case" )
+    else if ( selected_extension == ".case" )
     {
         if ( contains_wildcard( input_file_path.string() ) )
         {
@@ -215,7 +274,7 @@ int main( int argc, char** argv )
         }
         else
         {
-            Case2Kvsml( output_directory_path.string(), input_filename_without_extension.string(), input_file_path.string() );
+            Case2Kvsml( output_directory_path.string(), input_filename_without_extension, input_file_path.string() );
         }
     }
     else
